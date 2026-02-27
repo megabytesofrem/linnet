44bfaf03db460d5875c8fafc2b0dd89ae833744b
megabytesofrem <dhillonwilk@gmail.com>
2026-02-27 10:51:05 +0000

    Expression parsing

--- src/AST.hs
+++ src/AST.hs
@@ -1,1 +1,78 @@
-module AST where
+module AST
+  ( Ty (..),
+    Associativity (..),
+
+    -- * AST
+    UnaryOp (..),
+    BinOp (..),
+    Literal (..),
+    Expr (..),
+  )
+where
+
+data Ty
+  = TInt
+  | TFloat
+  | TBool
+  | TString
+  | TUnit
+  | TFn Ty Ty -- Function type, e.g. Int -> Int
+  | TVar String -- Type variable, e.g. a
+  | TForall String Ty -- forall a. a -> a
+  | TCons String [Ty] -- Type constructor with parameters
+  deriving (Show, Eq)
+
+-- The associativity of an operator, used in Parser.hs
+data Associativity
+  = AssocLeft
+  | AssocRight
+  | AssocNone
+  deriving (Show, Eq)
+
+data UnaryOp
+  = Negate -- -x
+  deriving (Show, Eq)
+
+{- ORMOLU_DISABLE -}
+data BinOp
+  = Add
+  | Sub
+  | Mul
+  | Div
+  | Mod
+  | Eq
+  | Neq
+  | Lt
+  | Gt
+  | LtEq
+  | GtEq
+  -- * Functional operators
+  | Apply   -- application: f x or f $ x
+  | Compose -- composition: f . g
+
+  | Bind    -- m >>= f
+  | Pipe    -- x >> f
+  | Map     -- f <$> x
+  | UFO     -- f <*> x
+  | Alt     -- x <|> y
+  deriving (Show, Eq)
+{- ORMOLU_ENABLE -}
+
+data Literal
+  = LitInt Integer
+  | LitFloat Double
+  | LitBool Bool
+  | LitString String
+  deriving (Show, Eq)
+
+data Expr
+  = ELit Literal
+  | EUnit -- ()
+  | EIdent String
+  | ELam [String] Expr -- \x y -> expr
+  | EUnaryOp UnaryOp Expr
+  | EBinOp BinOp Expr Expr
+  | EList [Expr] -- [1, 2, 3]
+  | ETuple [Expr] -- (1, 2, 3)
+  | EApp Expr Expr -- f x y z
+  deriving (Show, Eq)
--- src/Parser.hs
+++ src/Parser.hs
@@ -6,6 +6,7 @@ module Parser where
 
 -- AST
 import AST
+import Control.Monad.Combinators.Expr
 import Data.Void (Void)
 import Text.Megaparsec
 import Text.Megaparsec.Char
@@ -26,16 +27,22 @@ sc =
 lexeme :: Parser a -> Parser a
 lexeme = L.lexeme sc
 
-symbolP :: String -> Parser String
-symbolP = L.symbol sc
+-- | Parse something between parentheses, brackets, or braces.
+enclosed :: Char -> Char -> Parser a -> Parser a
+enclosed open close = between (char open >> sc) (char close >> sc)
 
--- Raw parsers
+symbol :: String -> Parser String
+symbol = L.symbol sc
+
+-- Primitive parsers
+
 identP :: Parser String
 identP = lexeme ((:) <$> letterChar <*> many alphas')
   where
     -- Allow ' and _ in identifiers
     alphas' = letterChar <|> alphaNumChar <|> char '\'' <|> char '_'
 
+-- Literals
 integerP :: Parser Integer
 integerP = lexeme L.decimal
 
@@ -44,3 +51,83 @@ floatP = lexeme L.float
 
 stringLitP :: Parser String
 stringLitP = lexeme (char '"' >> manyTill L.charLiteral (char '"'))
+
+literalP :: Parser Literal
+literalP =
+  choice
+    [ LitInt <$> integerP, -- 42
+      LitFloat <$> floatP, -- 3.14
+      LitString <$> stringLitP, -- "string literals"
+      LitBool True <$ symbol "true",
+      LitBool False <$ symbol "false"
+    ]
+
+-- Expression parser
+
+-- Lists and tuples: [1, 2, 3], (1, 2, 3)
+listP :: Parser Expr
+listP = enclosed '[' ']' (EList <$> sepBy exprP (symbol ","))
+
+tupleP :: Parser Expr
+tupleP = enclosed '(' ')' (ETuple <$> sepBy exprP (symbol ","))
+
+applicationP :: Parser Expr
+applicationP = do
+  f <- termP
+  args <- many termP
+  pure $ foldl EApp f args
+
+lambdaP :: Parser Expr
+lambdaP = do
+  _ <- symbol "\\"
+  binders <- some identP
+  _ <- symbol "->"
+  ELam binders <$> exprP
+
+-- Term parser
+termP :: Parser Expr
+termP =
+  choice
+    [ parens exprP,
+      listP,
+      tupleP,
+      lambdaP,
+      ELit <$> literalP,
+      EIdent <$> identP
+    ]
+  where
+    parens = enclosed '(' ')'
+
+-- Higher in the list = higher precedence.
+operatorTable :: [[Operator Parser Expr]]
+operatorTable =
+  [ [prefix "-" (EUnaryOp Negate)],
+    -- Left-associative binary operators
+    [binary "*" (EBinOp Mul) AssocLeft],
+    [binary "/" (EBinOp Div) AssocLeft],
+    [binary "+" (EBinOp Add) AssocLeft],
+    [binary "-" (EBinOp Sub) AssocLeft],
+    [binary "==" (EBinOp Eq) AssocNone],
+    [binary "/=" (EBinOp Neq) AssocNone],
+    [binary "<" (EBinOp Lt) AssocNone],
+    [binary ">" (EBinOp Gt) AssocNone],
+    [binary "<=" (EBinOp LtEq) AssocNone],
+    [binary ">=" (EBinOp GtEq) AssocNone],
+    -- Functional operators
+    [binary "." (EBinOp Compose) AssocRight],
+    [binary "$" (EBinOp Apply) AssocRight],
+    [binary ">>=" (EBinOp Bind) AssocLeft],
+    [binary ">>" (EBinOp Pipe) AssocLeft],
+    [binary "<$>" (EBinOp Map) AssocLeft],
+    [binary "<*>" (EBinOp UFO) AssocLeft],
+    [binary "<|>" (EBinOp Alt) AssocLeft]
+  ]
+  where
+    prefix sym f = Prefix (f <$ symbol sym)
+    binary sym f assoc = case assoc of
+      AssocLeft -> InfixL (f <$ symbol sym)
+      AssocRight -> InfixR (f <$ symbol sym)
+      AssocNone -> InfixN (f <$ symbol sym)
+
+exprP :: Parser Expr
+exprP = makeExprParser applicationP operatorTable
